25755b129fa2c5256f2ee5f3fbe850561057a772
east <694292191@qq.com>
2018-08-22 12:15:14 +0800

    init poem

--- Cordova/Demos/Poem/src/App.vue
+++ Cordova/Demos/Poem/src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <span>Welcome</span>
-    <HelloWorld/>
+    <Poem></Poem>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  import Poem from './components/Poem'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Poem,
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
